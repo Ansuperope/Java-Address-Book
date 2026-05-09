38bf660e950a5e292881c3efee8a07b46ab92399
Ansuperope <151668459+Ansuperope@users.noreply.github.com>
2026-05-09 00:44:25 +0000

    added files no code yet

--- project/Main.java
+++ project/Main.java
@@ -1,14 +1,18 @@
 /**
  * @file Main.java
- * @brief LOREM
  * 
  * @details
- * LOREM
+ * Build a console-based Address Book application that lets users manage
+ * different types of contacts. Users can add, edit, delete, search, 
+ * and list contacts, as well as save and load them from a file.
  */
 package project;
 
 // Include files from other folders
-import project.include.LOREM;
+import project.include.AddressBook;
+import project.include.Contact;
+import project.include.Formatter;
+import project.include.UIController;
 
 public class Main {
 
